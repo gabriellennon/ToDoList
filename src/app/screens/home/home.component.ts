374d07c4afbcef99ea2d8dd3be1175e7bba743b0
frontend <gabriellennon@wizsolucoes.com.br>
2022-01-30 20:45:52 -0300

    logic todo

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Task } from 'src/app/model/models.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //VARIABLES
+  listTasks: Task[] = [];
+
+  //FORMS
+  public taskForm: FormGroup = new FormGroup({
+    taskInpt: new FormControl('',  [Validators.required]),
+    taskDescription: new FormControl('',  [Validators.required]),
+  })
+
   constructor() { }
 
+  //FUNCTIONS
   ngOnInit(): void {
+  }
+
+  addNewTask(){
+    const valueFormInpt = this.taskForm.get('taskInpt').value;
+    const valueFormDescription = this.taskForm.get('taskDescription').value;
+    //Caso o formulário esteja válido e se não estiver vazio
+    if(this.taskForm.valid && valueFormInpt !== ''){
+      this.listTasks.push({
+        id: this.listTasks.length,
+        title: valueFormInpt,
+        description: valueFormDescription
+      })
+      console.log(this.listTasks)
+      this.resetForm();
+
+    }else {
+
+    }
+  }
+
+  removeTask(index: number){
+    console.log(index)
+    this.listTasks = this.listTasks.filter(e => e.id !== index);
+  }
+
+  resetForm(){
+    this.taskForm.patchValue({
+      taskInpt: '', 
+      taskDescription: ''
+    })
   }
 
 }
--- src/app/screens/home/home.module.ts
+++ src/app/screens/home/home.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialGeneralModule } from 'src/app/material.module';
 
 
 
@@ -9,7 +11,10 @@ import { HomeComponent } from './home.component';
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialGeneralModule
   ]
 })
 export class HomeModule { }
